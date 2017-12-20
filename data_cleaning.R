faa89f7d306ec007f010897e1123f9760b9aab07
Lauren Hallett <hallett@uoregon.edu>
2017-12-19 16:29:06 -0800

    update data_cleaning with last weeks work

---
+++ data_cleaning.R
@@ -0,0 +1,57 @@
+# load libraries to use
+library(tidyverse)
+
+# load the data
+# make sure you first set your working directory to where the files are stored!
+list.files()
+
+vegdat <- read.csv("veg plot data all.csv") %>%
+  tbl_df()
+
+
+head(vegdat)
+str(vegdat)
+
+sitekey <- read.csv("site type key.csv") %>%
+  tbl_df()
+
+# plantkey <- read.csv("plant list.csv") %>%
+#   tbl_df()
+# 
+# funckey <- read.csv("plant forms.csv") %>%
+#   tbl_df()
+
+## Rename the columns to a) remove spaces and b) standardizing the name of the column to join vegdat and sitekey by
+## vegdat previously had it as "Site.type" and sitekey as "site.type" - R is case sensitive and so these would not have joined
+names(vegdat)
+names(vegdat) = c("date", "newplotID", "observer", "site", "block", "sitetype", "precipblock", "newquadrat", "preciptrt", "code", "precinct", "comments",
+                  "count", "originalorder", "precinctcurrent", "year", "ID", "cover")
+names(vegdat)
+names(sitekey)
+names(sitekey) = c("sitetype", "grazetrt", "pasturetrt", "rodenttrt", "exclosure", "altsitetype")
+names(sitekey)
+
+
+unique(vegdat$code)
+
+
+# join the two dataframes. merge() does something similar in base R
+vegtog <- left_join(vegdat, sitekey) %>%
+  # remove columns we don't really need
+  # select() is a common function, we specify that we are using the one in dplyr
+  dplyr::select(-date, -observer) %>%
+  # remove bare and litter for now
+  # in base R subset() and which() do something similar
+  filter(code != "bare", code != "litter", code != "fresh dirt", code != "BARE", code != "HOLE", code != "bphole", code != "hole", code != "GOPHER", code != "MOSS",
+         code != "ANT", code != "LITTER", code != "FRESH DIRT") %>%
+  # make the case consistent for code
+  mutate(code = tolower(code)) %>%
+  # remove the swain 
+  filter(pasturetrt != "swain")%>%
+  filter(code != "cowpie")
+
+unique(vegtog$code)
+
+ourcheck <- vegtog %>%
+  filter(code == "")
+unique(vegtog$code)
