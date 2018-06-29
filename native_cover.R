e21afc5b191af17c07a828ebd12f86fb8972c5ec
Alejandro Brambila <abrambil@uoregon.edu>
2018-06-29 15:47:13 -0700

    aggregate across years and attempt mixed effects model

--- native_cover.R
+++ native_cover.R
@@ -10,113 +10,100 @@ theme_set(theme_bw(base_size = 16) + theme(text = element_text(size = 20)) +
                   panel.border = element_rect(colour = "black")))
 
 
-#calculate counts of native and invasive grasses and forbs by quadrat
-native_coverGF <- vegtog %>%
+#calculate percent cover of native and invasive grasses and forbs by quadrat
+natcover <- vegtog.sim %>%
   mutate(percover=count/81*100) %>%
   mutate(native=substr(form, 1, 1)) %>%
   mutate(native=as.factor(native)) %>%
   mutate(growthhabit=as.factor(growthhabit))%>%
   mutate(grazetrt=ifelse(year%in% c(2007,2012,2013,2014,2015) & grazetrt=="grazed", "gnograze", ifelse(year%in% c(2007,2012,2013,2014,2015) & grazetrt=="ungrazed", "nograze", grazetrt)))%>%
   group_by(year, site, quadrat, plot, precinct, grazetrt, growthhabit, native) %>%
-  summarize(nativecover=sum(percover)) %>%
+  summarize(percover=sum(percover)) %>%
   group_by(year, grazetrt, growthhabit, native) %>%
-  mutate(meancover=mean(nativecover), secover=calcSE(nativecover)) %>%
-  mutate(anlgrp=year) %>%
-  mutate(anlgrp=ifelse(native=="i", anlgrp+.1, anlgrp)) %>%
-  mutate(anlgrp=ifelse(growthhabit=="grass", anlgrp+.01, anlgrp)) %>%
-  mutate(anlgrp=ifelse(grazetrt=="grazed", anlgrp+.001, anlgrp)) %>%
-  mutate(anlgrp=anlgrp*1000)
+  filter(!is.na(native), !is.na(growthhabit))
+#average across quadrats within a year
+natcover.annual<-natcover %>%
+  group_by(year, grazetrt, native, growthhabit) %>%
+  summarize(meancover=mean(percover), secover=calcSE(percover))
 
-native_coverGF <- native_coverGF %>%
-  mutate(anlgrp=as.numeric(anlgrp)) %>%
-  mutate(anlgrp=as.character(anlgrp)) %>%
-mutate(anlgrp=as.factor(anlgrp))
-
-levels(native_coverGF$native) <- c("Invasive", "Native")
-levels(native_coverGF$growthhabit) <- c("Forb", "Grass")
-
-#Tukey test, tells us which groups are significantly different
-library(agricolae)
-percentcover.lm <- lm(nativecover ~ anlgrp, data = native_coverGF)
-percentcover.av <- aov(percentcover.lm)
-summary(percentcover.av)
-tukey.test <- TukeyHSD(percentcover.av)
-tukey.test
-plot(tukey.test)
-tukey.test2 <- HSD.test(percentcover.av, trt = 'anlgrp')
-tukey.test2
-tukeyresults <- tukey.test2$groups
-tukeyresults <- rownames_to_column(tukeyresults, var = "anlgrp") %>%
-  select(-nativecover)
-native_coverGF <- left_join(native_coverGF, tukeyresults)
-#covergroups gives each bar in the visualization a letter that puts it in a significance group
-covergroups <- native_coverGF %>%
-  group_by(year, grazetrt, growthhabit, native, meancover, secover, groups) %>%
-  summarize()
-
-### ALL GRAZED YEARS HAVE SIGNIFICANT DIFFERENCES EXCEPT NF2008 AND IF2009.
-#make a marker for NO significance to add to the plot
-sig_marker <- data.frame(
-  growthhabit= c("Forb", "Forb"),
-  native= c("Native", "Invasive"),
-  x = c(2008, 2009), y = c(70, 70))
+levels(natcover$native) <- c("Invasive", "Native")
+levels(natcover$growthhabit) <- c("Forb", "Grass")
+#levels(natcover$precinct) <-c("Off-Precinct", "Precinct", "Precinct")
 
 #Native and invasive Grasses and forbs %cover effect from grazing
-ggplot(subset(native_coverGF), aes(year, meancover)) + 
+ggplot(subset(natcover.annual), aes(year, meancover)) + 
   geom_bar(aes(fill=grazetrt), stat="identity", position="dodge") +
   geom_errorbar(aes(ymin=(meancover-secover), ymax=meancover+secover, group=grazetrt), position="dodge", color = "black", lwd = .1) +
   facet_grid(growthhabit~native)+
   scale_fill_manual("Result", values = c("grey","brown", "grey", "darkblue"), 
                     labels = c("No Grazing", " Grazed", "No Grazing", "Ungrazed")) +
   labs(x="Year",y="Percent Cover") +
-  annotate("segment", x = 2012, xend = 2012, y = 65, yend = 46, colour = "black", size=.5, alpha=1, arrow=arrow(type="closed")) + 
-  theme(legend.position = "none")+ geom_text(
-    data    = sig_marker,
-    mapping = aes(x = x, y = y, label = "*"), size=15)
+  annotate("segment", x = 2012, xend = 2012, y = 65, yend = 46, colour = "black", size=.5, alpha=1, arrow=arrow(type="closed"))+ 
+  theme(legend.position = "none")
 
-
-
-
-
-
-
-#counts of native and invasive grasses with and without grazing,
-#grazed years are 2008-2011, 2016-2017
-ggplot(subset(native_coverGF, growthhabit=="grass"), aes(year, meancover)) + 
+#Just native and invasives % cover effect from grazing
+ggplot(subset(natcover.annual), aes(year, meancover)) + 
   geom_bar(aes(fill=grazetrt), stat="identity", position="dodge") +
   geom_errorbar(aes(ymin=(meancover-secover), ymax=meancover+secover, group=grazetrt), position="dodge", color = "black", lwd = .1) +
-   scale_color_brewer() +facet_wrap(~native)+
+  facet_grid(~native)+
   scale_fill_manual("Result", values = c("grey","brown", "grey", "darkblue"), 
                     labels = c("No Grazing", " Grazed", "No Grazing", "Ungrazed")) +
   labs(x="Year",y="Percent Cover") +
-  annotate("segment", x = 2012, xend = 2012, y = 55, yend = 46, colour = "black", size=.5, alpha=1, arrow=arrow(type="closed")) + theme(legend.position = "none")
+  annotate("segment", x = 2012, xend = 2012, y = 65, yend = 46, colour = "black", size=.5, alpha=1, arrow=arrow(type="closed"))+ 
+  theme(legend.position = "none")
+
+#aggregated across years grouped by grazing, native, growthhabit
+natcover.agg <- natcover %>%
+  filter(grazetrt=="grazed"|grazetrt=="ungrazed") %>%
+  group_by(grazetrt, growthhabit, native, precinct) %>%
+    summarize(meancover=mean(percover), secover=calcSE(percover))
+
+ggplot(natcover.agg, aes(native, meancover)) + 
+  geom_bar(aes(fill=grazetrt), stat="identity", position="dodge") +
+  facet_grid(precinct~growthhabit) +
+  geom_errorbar(aes(ymin=(meancover-secover), ymax=meancover+secover, group=grazetrt), position="dodge", color = "black", lwd = .1) +
+  scale_fill_manual("Result", values = c("brown","darkblue"))+
+  labs(x="",y="Percent Cover") + 
+  theme(legend.position = "none")
 
 
+ggplot(natcover) + 
+  geom_bar(aes(x=native, y= mean(percover), group=native, fill=grazetrt), stat="identity", position="dodge", group="grazetrt") #+
+  facet_grid(precinct~growthhabit) +
+  geom_errorbar(aes(ymin=(meancover-secover), ymax=meancover+secover, group=grazetrt), position="dodge", color = "black", lwd = .1) +
+  scale_fill_manual("Result", values = c("brown","darkblue"))+
+  labs(x="",y="Percent Cover") + 
+  theme(legend.position = "none")
 
 
+##MIXED EFFECTS MODEL
+library(lme4)
+grazing.model <- lmer(meancover~grazetrt*native + (1|year) + (1|plot), data=subset(natcover, grazetrt=="grazed"|grazetrt=="ungrazed"), REML=FALSE)
+summary(grazing.model)
+
+#Look for interaction effect
+grazing.null <- lmer(meancover~grazetrt+native + (1|year) + (1|plot), data=subset(natcover, grazetrt=="grazed"|grazetrt=="ungrazed"), REML=FALSE)
+anova(grazing.null,grazing.model)
 
 
-
-### MORE ANALYSIS WITH PRECINCT, ETC
+### PRECINCT INTERACTION FOR INVASIVE GRASSES
 #redo including precincts
-native_coverGFfilter <- vegtog %>%
-  # filter(!is.na(cover)) %>%
+natcover.p <- vegtog %>%
   mutate(native=substr(form, 1, 1)) %>%
   group_by(year, site, quadrat, plot, precinct, grazetrt, growthhabit, native) %>%
   summarize(nativecount=sum(count)) %>%
   group_by(year, precinct, grazetrt, growthhabit, native) %>%
   mutate(meancount=mean(nativecount), secount=calcSE(nativecount)) %>%
-  filter(growthhabit=="grass", native=="i") 
+  filter(growthhabit=="grass", native=="i")
 
 #Count invasive grasses with and without grazing, on and off mound,
 #grazed years are 2008-2011, 2016-2017
-ggplot(subset(native_coverGFfilter), aes(year, meancount)) + 
-  geom_point(aes(color=grazetrt)) +
-  geom_line(aes(color=grazetrt)) +
-  geom_errorbar(aes(ymin=(meancount-secount), ymax=meancount+secount, color=grazetrt)) +
-  theme_bw() + scale_color_brewer(palette="Dark2") +facet_wrap(~precinct)
+ggplot(data=natcover.p, aes(year, meancount)) + 
+  geom_bar(aes(fill=grazetrt), stat="identity", position="dodge") +
+  geom_errorbar(aes(ymin=(meancount-secount), ymax=meancount+secount, group=grazetrt),position="dodge", color = "black", lwd = .1) +
+  facet_wrap(~precinct)
 
-#calculate counts of total native and invasive plants by quadrat
+#AGGREGATED NATIVES AND INVASIVES
 native_cover<- vegtog %>%
  # filter(!is.na(cover)) %>%
   mutate(native=substr(form, 1, 1)) %>%
