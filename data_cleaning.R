7503c24e70fa6da296f94e579ce4bf788d3fe80b
Alejandro Brambila <abrambil@uoregon.edu>
2018-12-20 11:22:52 -0500

    moved some cleaning from other sub-scripts into this one to keep it the master cleaning file.
    pretty much created trends file from the ground up.  see notation within file.

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,54 +1,64 @@
-## This script exists to bring in all the Carrizo data and merge it into a master dataframe, vegtog.
-## It also aims to clean the data taking out unnecessary columns, arranging them and filtering irrelevant
-## or bad observations. 
+######################################
+## CARRIZO DATA IMPORT AND CLEANING ##
+######################################
 
-# load libraries to use
+## Bring in all the Carrizo data and merge it into a master dataframe, vegtog.
+## Clean the data, taking out unnecessary columns, arranging them and filtering observations.
+
+## Data will be used to investigate the effect of cattle grazing on vegetation dynamics.
+
+## Run this script before running any other script in the project.  
+## They all require this script, but not each other.   
+
 library(tidyverse)
 library(vegan)
-
+library(acrigolae)
+library(ggpubr)
 
 ### LOAD THE DATA ###
-#Collected vegetation data from pin drops
-vegdat <- read.csv("veg plot data all.csv")
-#treatment for each site type in vegdat
-sitekey <- read_csv("site type key.csv")
-#what species the species codes in vegdat refer to
-plantkey <- read_csv("plant list.csv")
-#functional characteristics of species in plant list
-funckey <- read_csv("plant forms.csv")
-#biomass per quadrat
-biomass <- read_csv("veg plot biomass all years stacked.csv")
-#cowpies per plot
-cowpies <- read_csv("cowpie counts all years.csv")
-
+vegdat <- read.csv("veg plot data all.csv") # Collected vegetation data from pin drops: 81 drops/quadrat
+sitekey <- read_csv("site type key.csv")    # treatment for each site type in vegdat
+plantkey <- read_csv("plant list.csv")      # species codes interpreted
+funckey <- read_csv("plant forms.csv")      # associated plant functional characteristics
+biomass <- read_csv("veg plot biomass all years stacked.csv") # biomass per quadrat
+cowpies <- read_csv("cowpie counts all years.csv")            # cowpies per plot
 
 
 ### JOIN VEGDAT AND SITE KEY TO CREATE VEGTOG ###
 
-#standardize column names:
-names(vegdat) = c("date", "quadrat", "observer", "site", "plot", "sitetype", "precipblock", "newquadrat", "preciptrt", "code", "precinct", "comments",
+names(vegdat) = c("date", "quadrat", "observer", "site", "plot", "sitetype", "precipblock", 
+                  "newquadrat", "preciptrt", "code", "precinct", "comments",
                   "count", "originalorder", "precinctcurrent", "year", "ID", "cover")
 names(sitekey) = c("sitetype", "grazetrt", "pasturetrt", "rodenttrt", "exclosure", "altsitetype")
+
 vegtog <- left_join(vegdat, sitekey) %>%
   select(-date, -observer, -comments, -originalorder, ID, -altsitetype) %>%
-  filter(code != "bare", code != "litter", code != "fresh dirt", code != "BARE", code != "HOLE", code != "bphole", code != "hole", code != "GOPHER", code != "MOSS",
-         code != "ANT", code != "LITTER", code != "FRESH DIRT", code != "cowpie", code != "") %>%
+  filter(code != "bare", code != "litter", code != "fresh dirt", code != "BARE", code != "HOLE", 
+         code != "bphole", code != "hole", code != "GOPHER", code != "MOSS", code != "ANT", 
+         code != "LITTER", code != "FRESH DIRT", code != "cowpie", code != "") %>%
   mutate(code = tolower(code)) %>%
   mutate(precipblock = tolower(precipblock)) %>%
-  # remove the swain pasture that was never grazed
-  filter(pasturetrt != "swain")%>%
-  # remove rat exclosures in the remaining area, as well as species with no counts or NA counts.
-  filter (count !=0, !is.na(count), exclosure != "CW rat excl")
+  filter(pasturetrt != "swain")%>%                               # remove the ungrazed "swain" pasture
+  filter (count !=0, !is.na(count), exclosure != "CW rat excl") %>% # remove GKR exclosure plots and 0/NA 
+  mutate(precinct=as.character(precinct)) %>%
+  mutate(precinct=ifelse(precinct=="P ","P", precinct)) %>%
+  mutate(precinct=as.factor(precinct)) %>%
+  mutate(year=as.integer(year))
 
 
 ### JOIN VEGTOG AND PLANTKEY ###
 #Standardize column names
-names(plantkey) = c("plantID", "family", "commonfamily", "shortcode", "code", "binomial", "genus", "species", "variety", "synonym", "common", "sink","salt",
-                    "grass","desert","juniper","oak","seep","oldform","soda","central","western","mountain","elkhorn","teblor","flowermonth","flowercolor",
+names(plantkey) = c("plantID", "family", "commonfamily", "shortcode", "code", "binomial", 
+                    "genus", "species", "variety", "synonym", "common", "sink","salt",
+                    "grass","desert","juniper","oak","seep","oldform","soda","central",
+                    "western", "mountain","elkhorn","teblor","flowermonth","flowercolor",
                     "native","gkrprefer","refcode","comments", "form")
+
 plantkey <- dplyr::select(plantkey, -shortcode, -sink, -salt, -grass, -desert, -juniper, -oak, 
                           -seep, -soda, -central, -western, -mountain, -elkhorn, -teblor, binomial, 
-                          -commonfamily, -oldform, -common, -genus, -species, -flowercolor, -comments, -variety, -synonym, -family)
+                          -commonfamily, -oldform, -common, -genus, -species, -flowercolor, -comments, 
+                          -variety, -synonym, -family)
+
 vegtog <- left_join(vegtog, plantkey, by="code")
 
 
@@ -64,19 +74,20 @@ biomass <- biomass %>%
   select("year", "Site type", "Block", "New Plot ID", "net weight", "season")
 names(biomass) = c("year", "sitetype", "plot", "quadrat", "netwt", "wtmonth")
 biomass <- biomass %>%
+  mutate(wtmonth = tolower(wtmonth)) %>%
   filter(sitetype=="CR"|sitetype=="EP", !is.na(year), !is.na(plot), !is.na(netwt), !is.na(quadrat), !is.na(wtmonth))%>%
-  mutate(wtmonth=ifelse(wtmonth=="Spring", "April", wtmonth)) %>%
-  spread(wtmonth, netwt) %>%
-  select(-APRIL, -october, -april) ## We lose All the June measurements because their quad #s are 1-8 not "NO215" etc.
+  mutate(wtmonth=ifelse(wtmonth=="spring", "april", wtmonth))  %>%
+  filter(wtmonth!="june") %>% ## We lose All the June measurements because their quad #s are 1-8 not "NO215" etc.
+  spread(wtmonth, netwt)
 
 
-#join (this doubles all entries in vegtog, giving them a spring and fall weight row)
+#join april and october biomass columns to vegtog
 vegtog <- left_join(vegtog, biomass)
 names(vegtog)
 
 ### JOIN VEGTOG AND COWPIES ###
 
-#clean up and rename cowpies
+#clean up and rename cowpies (plot/block level)
 cowpies <-cowpies %>%
   select(year, "alt site type", block, cowpies)
 names(cowpies)<-c("year", "type","block", "cowpies")
@@ -99,25 +110,33 @@ vegtog<- vegtog %>%
   select(-sitetype, -precipblock, -newquadrat, -ID, -pasturetrt, -rodenttrt, -plantID, -exclosure)
 
 vegtog <- vegtog[c("year", "site", "plot", "quadrat", "precinct", "precinctcurrent", "preciptrt", 
-                   "grazetrt", "cowpies", "netwt", "wtmonth", "code", "binomial", "count", "cover", "flowermonth", 
+                   "grazetrt", "cowpies", "april", "october", "code", "binomial", "count", "cover", "flowermonth", 
                    "form", "fullform", "lifecycle", "growthhabit")]
 
 
-#remove precip treatments that have irrigation or shelter.  get rid of other unnecessary columns
+# remove precip treatments that have irrigation or shelter
 vegtog <- vegtog %>%
   mutate(preciptrt=tolower(preciptrt)) %>%
-  filter(preciptrt!="irrigation", preciptrt!="shelter", !is.na(binomial)) %>%
-  #get rid of anything in precinct current that is "NOT OK"
+  filter(preciptrt!="irrigation", preciptrt!="shelter", !is.na(binomial))
+
+# remove quadrats that have shifted between on off mound.
+current<-count(vegtog, precinctcurrent) %>%
+  mutate(precinctcurrent=tolower(precinctcurrent))%>%
+  filter(n<150) %>%
+  filter(!(row_number() %in% c(20, 21, 30, 31, 33, 34, 35)))
+current<-current$precinctcurrent
+
+vegtog <- vegtog %>%
   mutate(precinctcurrent=tolower(precinctcurrent)) %>%
-  filter(precinctcurrent!="n not ok", precinctcurrent!="n not ok. active pruning.", 
-         precinctcurrent!="n not okay", precinctcurrent!="not ok", precinctcurrent!="p not ok", 
-         precinctcurrent!="p not okay")%>%
-  select(-June)
+  filter(!(precinctcurrent %in% current)) %>%
+  select(-precinctcurrent, -preciptrt, -cover)%>%
+  mutate(native=substr(form, start=1, stop=1))
 
 # Clean up environment.
-rm(cowpies, funckey, plantkey, sitekey, vegdat, biomass)
+rm(cowpies, funckey, plantkey, sitekey, vegdat, biomass, current)
 
 names(vegtog)
+str(vegtog)
 
 ##FN for Calculating SE
 calcSE<-function(x){
@@ -125,4 +144,10 @@ calcSE<-function(x){
   sd(x)/sqrt(length(x))
 }
 
-str(vegtog)
+# set ggplot2 theme
+theme_set(theme_bw(base_size = 12) + theme(text = element_text(size = 14)) +
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  strip.background = element_blank(),
+                  panel.border = element_rect(colour = "black"),
+                  legend.title=element_blank()))
--- trends.R
+++ trends.R
@@ -1,9 +1,285 @@
-##Look at trends of richness over time.
+######################################
+## TRENDS / INTERANNUAL VARIABILITY ##
+######################################
 
-#Total number of species by graze treatment over the years (ignoring plots)
-trends <- vegtog%>%
-  group_by(year, grazetrt) %>%
-  summarize(richness=length(unique(plantID)))
-ggplot(trends, aes(x=year, y=richness, color=grazetrt))+ geom_line() #+ geom_smooth(aes(color=grazetrt))
+## Look at trends of richness, cover, biomass over time. 
+## Demonstrate that interannual variability is a major structuring force in this system.
+## We will need to deal with this to go forwards
+## In these visualizations ungrazed drought years are included to show 
+## consistency of pattern across trt. 
 
+## Part 1: Richness trends at three levels
+## Part 2: Cover trend
+## Part 3: Biomass trend
+## Part 4: Functional group trends
+## Part 5: Species invasion trends (proportion and accumulation)
+
+
+################################################
+# Part 1: RICHNESS TRENDS AT THREE LEVELS:     #
+# Full site, 20 paired plots, and 173 quadrats #
+################################################
+
+# Total 
+annUniqueSp <- vegtog%>%  
+  group_by(year, grazetrt, precinct) %>%
+  summarize(richness=length(unique(code)))
+# Plot-Level
+gammaUniqueSp<- vegtog%>%
+  group_by(year, grazetrt, precinct, site) %>%
+  summarize(richness=length(unique(code))) %>%
+  group_by(year, grazetrt, precinct) %>%
+  summarize(meanrich=mean(richness), SE=calcSE(richness))
+# Quadrat-Level 
+alphaUniqueSp <- vegtog%>%
+  group_by(year, grazetrt, precinct, quadrat) %>%
+  summarize(richness=length(unique(code))) %>%
+  group_by(year, grazetrt, precinct) %>%
+  summarize(meanrich=mean(richness), SE=calcSE(richness))
+
+# Visualize diversity trends in one figure. 
+p1<-ggplot(annUniqueSp, aes(x=year, y=richness, color=grazetrt))+ 
+  geom_line() + 
+  facet_wrap(~precinct) +
+  ylab("Total Unique Species") +
+  xlab("") + facet_wrap(~precinct) +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=30, alpha = .2)+
+  annotate("text", x=2013.5, y=25, label="not grazed", alpha=.6)
+p2<-ggplot(gammaUniqueSp, aes(x=year, y=meanrich, color=grazetrt))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=meanrich-SE, ymax=meanrich+SE)) +
+  facet_wrap(~precinct) +
+  ylab("Unique Species/Plot") +
+  xlab("") + facet_wrap(~precinct) +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=15, alpha = .2)+
+  annotate("text", x=2013.5, y=13, label="not grazed", alpha=.6)
+p3<-ggplot(alphaUniqueSp, aes(x=year, y=meanrich, color=grazetrt))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=meanrich-SE, ymax=meanrich+SE)) +
+  facet_wrap(~precinct) +
+  ylab("Unique Species/Quadrat") +
+  xlab("Year") + facet_wrap(~precinct) +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=9, alpha = .2)+
+  annotate("text", x=2013.5, y=8, label="not grazed", alpha=.6)
+ggarrange(p1, p2, p3, nrow=3, ncol=1, common.legend = TRUE)
+
+
+#######################
+# Part 2: COVER TREND #
+#######################
+
+alphaCover<- vegtog%>%
+  group_by(year, grazetrt, precinct, quadrat) %>% 
+  summarize(stems=sum(count))%>% #count by quad
+  group_by(year, grazetrt, precinct)%>% #average all quads for the year
+  summarize(mean=mean(stems), se=calcSE(stems))
+ggplot(alphaCover, aes(x=year, y=mean, color=interaction(grazetrt, precinct)))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=mean-se, ymax=mean+se)) +
+  ylab("Hits/81/Quad") +
+  xlab("Year") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=80, alpha = .2)+
+  annotate("text", x=2013.5, y=75, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))
+
+
+#########################
+# Part 3: BIOMASS TREND #
+#########################
+
+gammaBiomass<- vegtog%>% # measured at the plot level, not quadrat
+  filter(!is.na(april), !is.na(october))%>%
+  group_by(year, grazetrt, precinct) %>% 
+    summarize(mean=mean(october), se=calcSE(october))
+ggplot(gammaBiomass, aes(x=year, y=mean, color=interaction(grazetrt, precinct)))+ 
+  geom_line() +
+ geom_errorbar(aes(x=year, ymin=mean-se, ymax=mean+se)) +
+  ylab("Biomass/Plot") +
+  xlab("Year") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=30, alpha = .2)+
+  annotate("text", x=2013.5, y=25, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))
+
+###################################
+# Part 4: FUNCTIONAL GROUP TRENDS #
+###################################
+
+#General Grass Trend
+totalStems<- vegtog%>%
+  group_by(year, grazetrt, precinct, quadrat) %>% 
+  summarize(stems=sum(count))
+fgTrend<-subset(vegtog, growthhabit=="grass")%>%
+  group_by(year, grazetrt, quadrat, site, precinct) %>%
+  summarize(grasscount=sum(count))
+fgTrend<-left_join(totalStems, fgTrend)
+fgTrend[is.na(fgTrend)] <- 0  
+fgTrend<-fgTrend%>%
+  group_by(year, grazetrt, precinct)%>% #average all quads for the year
+  mutate(prop=grasscount/stems)%>%
+  summarize(mean=mean(prop), se=calcSE(prop))
+#Visualize
+ggplot(fgTrend, aes(x=year, y=mean, color=interaction(grazetrt, precinct)))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=mean-se, ymax=mean+se)) +
+  ylab("Proportion Grass (excluding bare)") +
+  xlab("Year") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=1, alpha = .2)+
+  annotate("text", x=2013.5, y=.95, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))
+  
+
+# Invasive Grasses Trend
+igTrend<-subset(vegtog, native=="i"&growthhabit=="grass")%>%
+  group_by(year, grazetrt, quadrat, site, precinct) %>%
+  summarize(count=sum(count))
+igTrend<-left_join(totalStems, igTrend)
+igTrend[is.na(igTrend)] <- 0  
+igTrend<-igTrend%>%
+  group_by(year, grazetrt, precinct)%>% #average all quads for the year
+  mutate(prop=count/stems)%>%
+  summarize(mean=mean(prop), se=calcSE(prop))
+#Visualize
+ggplot(igTrend, aes(x=year, y=mean, color=interaction(grazetrt, precinct)))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=mean-se, ymax=mean+se)) +
+  ylab("Proportion iGrasses (excluding bare)") +
+  xlab("Year") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=1, alpha = .2)+
+  annotate("text", x=2013.5, y=.95, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))
+
+
+# Invasive Forbs Trend (Compensatory foil for I.G. in drought)
+ifTrend<-subset(vegtog, native=="i"&growthhabit=="forb")%>%
+  group_by(year, grazetrt, quadrat, site, precinct) %>%
+  summarize(count=sum(count))
+ifTrend<-left_join(totalStems, ifTrend)
+ifTrend[is.na(ifTrend)] <- 0  
+ifTrend<-ifTrend%>%
+  group_by(year, grazetrt, precinct)%>% #average all quads for the year
+  mutate(prop=count/stems)%>%
+  summarize(mean=mean(prop), se=calcSE(prop))
+#Visualize
+ggplot(ifTrend, aes(x=year, y=mean, color=interaction(grazetrt, precinct)))+ 
+  geom_line() +
+  geom_errorbar(aes(x=year, ymin=mean-se, ymax=mean+se)) +
+  ylab("Proportion iForbs (excluding bare)") +
+  xlab("Year") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=0, ymax=1, alpha = .2)+
+  annotate("text", x=2013.5, y=.95, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))
+
+
+###############################################################
+# Part 5: SPECIES INVASION TRENDS (Hordeum, Schismus, Bromus) #
+###############################################################
+
+#General quadrat attrition
+quadCount<-vegtog%>%
+  group_by(year, grazetrt, precinct) %>%
+  summarize(totquadcount=length(unique(quadrat))) 
+#Visualize, we have to use proportions not total# of quads:
+ggplot(quadCount, aes(x=year, y=totquadcount, color=interaction(grazetrt, precinct))) +
+  geom_line() +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=.25, ymax=48, alpha = .2)+
+  annotate("text", x=2013.5, y=45, label="not grazed", alpha=.6)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))+
+  labs(y="# of Quadrats")
+
+## PROPORTION OF QUADRATS WITH KEY WEEDS
+HquadCount <- subset(vegtog, vegtog$code == "hormur") %>%
+  group_by(year, grazetrt, precinct) %>%
+  mutate(hquadcount=length(unique(quadrat))) %>%
+  group_by(year, grazetrt, precinct, hquadcount) %>%
+  summarize()
+SquadCount <- subset(vegtog, vegtog$code == "schara") %>%
+  group_by(year, grazetrt, precinct) %>%
+  mutate(squadcount=length(unique(quadrat))) %>%
+  group_by(year, grazetrt, precinct, squadcount) %>%
+  summarize()
+BquadCount <- subset(vegtog, vegtog$code == "bromad") %>%
+  group_by(year, grazetrt, precinct) %>%
+  mutate(bquadcount=length(unique(quadrat))) %>%
+  group_by(year, grazetrt, precinct, bquadcount) %>%
+  summarize()
+quadCount<-left_join(quadCount, HquadCount)
+quadCount<-left_join(quadCount, BquadCount)
+quadCount<-left_join(quadCount, SquadCount)
+quadCount[is.na(quadCount)] <- 0 
+quadProp<-quadCount %>%
+  mutate(hormur=hquadcount/totquadcount, 
+         bromad=bquadcount/totquadcount, 
+         schara=squadcount/totquadcount) %>%
+  select(-hquadcount, -totquadcount, -squadcount, -bquadcount)
+qpLong<-gather(quadProp, key="species", value="propQuads", 
+               hormur, bromad, schara)
+#Visualize
+ggplot(qpLong, aes(x=year, y=propQuads, color=interaction(grazetrt, precinct))) +
+  geom_line() +
+  facet_wrap(~species, scales="free") +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=.4, ymax=.5, alpha = .2)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue"))+
+  labs(y="Proportion of Quadrats")
+
+
+
+## QUADRAT ACCUMULATIONS: i.e. how many quadrats has the species ever been seen in?
+weedSpread <- subset(vegtog, code=="hormur"|code=="schara"|code=="bromad") %>%
+  select(year, quadrat, site, grazetrt, precinct, code) %>%
+  group_by(quadrat, site, code, grazetrt, precinct) %>%
+  summarize(firstyear=min(year)) %>% #calculate first year a species was seen in each quadrat
+  group_by(firstyear, grazetrt, precinct, code) %>%
+  summarize(newquads=n())%>% #how many new quadrats were added that year
+  ungroup()%>%
+  mutate(firstyear=as.character(firstyear))%>%
+  mutate(firstyear=paste("y", firstyear, sep=""))%>%
+  spread(key=firstyear, value=newquads, fill=0)%>%
+  mutate(y2008=y2008+y2007, y2009=y2009+y2008, y2010=y2010+y2009,
+         y2011=y2011+y2010, y2012=y2012+y2011, y2013=y2013+y2012,
+         y2014=y2014+y2013, y2015=y2015+y2014, y2016=y2016+y2015,
+         y2017=y2017+y2016)%>%  #add all previous years to each years' new count
+  gather(key="year", value="numQuads", 
+         y2007, y2008, y2009, y2010, y2011, y2012,
+         y2013, y2014, y2015, y2016, y2017) %>%
+  mutate(year=substr(year, start=2, stop=5), year=as.integer(year))
+# Repeat by site
+weedSpreadG <- subset(vegtog, code=="hormur"|code=="schara"|code=="bromad") %>%
+  select(year,  site, grazetrt, precinct, code) %>%
+  group_by( site, code, grazetrt, precinct) %>%
+  summarize(firstyear=min(year)) %>%
+  group_by(firstyear, grazetrt, precinct, code) %>%
+  summarize(newsite=n())%>%
+  ungroup()%>%
+  mutate(firstyear=as.character(firstyear))%>%
+  mutate(firstyear=paste("y", firstyear, sep=""))%>%
+  spread(key=firstyear, value=newsite, fill=0)%>%
+  mutate(y2008=y2008+y2007, y2009=y2009+y2008,
+         y2011=y2011+y2009, y2012=y2012+y2011, y2016=y2016+y2012,
+         y2017=y2017+y2016)%>%
+  gather(key="year", value="numSite", 
+         y2007, y2008, y2009, y2011, y2012,
+         y2016, y2017) %>%
+  mutate(year=substr(year, start=2, stop=5), year=as.integer(year))
+
+
+#Visualize quadrat accumulation curves
+ggplot(weedSpread) + 
+  geom_line(aes(x=year, y=numQuads,
+                color=interaction(grazetrt, precinct))) +
+  facet_wrap(~code)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue")) +
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=5, ymax=52, alpha = .2)+
+  labs(y="Number of unique quadrats ever occupied", x="Year")+
+  ggtitle("Weed spread though quadrat accumulation")
+
+
+#Visualize site accumulation curves
+ggplot(weedSpreadG) + 
+  geom_line(aes(x=year, y=numSite,
+                color=interaction(grazetrt, precinct))) +
+  facet_wrap(~code)+
+  scale_color_manual(values=c("pink", "brown", "lightblue", "darkblue")) +
+  labs(y="Number of unique sites ever occupied", x="Year")+
+  annotate("rect", xmin = 2011.5, xmax = 2015.5, ymin=4, ymax=10, alpha = .2)+
+  ggtitle("Weed spread though site accumulation")
 
